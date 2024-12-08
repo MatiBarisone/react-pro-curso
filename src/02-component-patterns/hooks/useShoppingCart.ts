730df380e0b288ba499e08fbcdbe142839a73ebb
Mati Barisone <62723688+MatiBarisone@users.noreply.github.com>
2024-12-08 14:27:05 -0300

    Control Props Altenativo  Implementado

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -7,22 +7,7 @@ export const useShoppingCart = () => {
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
 
         setShoppingCart(oldShoppingCart => {
-
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }
-
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
-            }
-
-            //Borrar el producto:
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest
-
-            /*             
+   
             if (count === 0) {
                 const { [product.id]: toDelete, ...rest } = oldShoppingCart;
                 return rest
@@ -32,7 +17,7 @@ export const useShoppingCart = () => {
                 ...oldShoppingCart,
                 [product.id]: { ...product, count }
             } 
-            */
+            
         })
 
     }
